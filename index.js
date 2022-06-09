2d4e01f596046ceb7d102f6ee2bc52a876dc5e14
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-09 18:19:49 -0400

    Added fetchCoordsByIP call

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require("./iss");
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
+//   console.log('It worked! Returned IP:' , ip);
+// });
 
-  console.log('It worked! Returned IP:' , ip);
-});
+// fetchCoordsByIP("174.89.152.46d", (error, data) => {
+//   if (error) {
+//     console.log("Uh oh!" , error.message);
+//     return;
+//   } else {
+//     console.log('Success! Returned coordinates:' , data);
+//   }
+// });
