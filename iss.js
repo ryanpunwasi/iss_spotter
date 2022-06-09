1b84b3a63d5dc78f90c20e9c84abaef160bbf76f
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-09 18:20:15 -0400

    Implemented fetchCoordsByIP

--- iss.js
+++ iss.js
@@ -8,14 +8,39 @@
  */
 
 const request = require('request');
-const fetchMyIP = function(callback) { 
-  request('https://api.ipify.org/?format=json', (err, resp, body) => {
-    if(!err) {
-      callback(null, JSON.parse(body).ip);
-    } else {
+const { isBuffer } = require('util');
+const fetchMyIP = function(callback) {
+  request('https://api.ipify.orkg/?format=json', (err, resp, body) => {
+    if (err) {
       callback(err, null);
+    } else if (resp.statusCode !== 200) {
+      const msg = `Status Code ${resp.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    } else {
+      callback(null, JSON.parse(body).ip);
     }
   });
-}
+};
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = (ip, cb) => {
+  request(`https://api.ipbase.com/v2/info?apikey=4HR4YD5gvQmsQJFI32x86BmHklxPxiC7XZuUJRIZ&ip=${ip}`, (err, resp, body) => {
+    if(err) {
+      cb(err, null);
+      return;
+    } else if (resp.statusCode !== 200) {
+      const msg = `Status Code ${resp.statusCode} when fetching coordinates for IP. Response: ${JSON.parse(body).message}`;
+      cb(Error(msg), null);
+      return;
+    } else {
+      const payload = JSON.parse(body);
+      const coords = { 
+        latitude: payload.data.location.latitude,
+        longitude: payload.data.location.longitude
+      };
+      cb(null, coords);
+    }
+  }
+)};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
